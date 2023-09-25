34bbd50ff0fe48f8d84dc07666e84e3f9cd24d51
luizatorquatto <133678662+luizatorquatto@users.noreply.github.com>
2023-09-25 12:37:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function pom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = pom;
+function clap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = clap;
+function tim(){
+    document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim').onclick = tim;
